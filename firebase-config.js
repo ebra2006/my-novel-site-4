b6fc7728edc22eeab7448100b0319c4f43bac4e1
ebra2006 <ebrahimzaid02@gmail.com>
2025-03-20 00:10:12 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,6 @@
+// استيراد مكتبات Firebase المطلوبة
 import { initializeApp } from "firebase/app";
-
-
-
 import { getFirestore } from "firebase/firestore";
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
-
-
-
-
-
 import { getAnalytics } from "firebase/analytics";
 
 // تعريف المتغيرات البيئية
@@ -29,44 +17,7 @@ const firebaseConfig = {
 // تهيئة Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export { app, analytics };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app); // إضافة Firestore
-
+// تصدير القيم لاستخدامها في الملفات الأخرى
 export { app, analytics, db };
