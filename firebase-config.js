ad6082c149a8bcafc05f5dde7cff0e29a30cde18
ebra2006 <ebrahimzaid02@gmail.com>
2025-03-19 21:43:43 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,18 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// تعريف المتغيرات البيئية
 const firebaseConfig = {
-  apiKey: "AIzaSyDVgX5siwAvifImpq4_u7T2-s3Wv0oso3g",
-  authDomain: "veronnovel-fd5c8.firebaseapp.com",
-  projectId: "veronnovel-fd5c8",
-  storageBucket: "veronnovel-fd5c8.firebasestorage.app",
-  messagingSenderId: "581899832569",
-  appId: "1:581899832569:web:423e84bb20e1aa54856628",
-  measurementId: "G-PK4WPW2LVB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
+// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export { app, analytics };
