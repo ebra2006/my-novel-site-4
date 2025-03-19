dc43c0ca9655bc24cef16102e03605359a9dcb33
ebra2006 <ebrahimzaid02@gmail.com>
2025-03-19 23:17:15 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,18 @@
 import { initializeApp } from "firebase/app";
+
+
+
+import { getFirestore } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
+
+
+
+
+
 import { getAnalytics } from "firebase/analytics";
 
 // تعريف المتغيرات البيئية
