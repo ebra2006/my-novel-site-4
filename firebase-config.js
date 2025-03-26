a303532666998e6d4778294497580ca3d78f8120
ebra2006 <ebrahimzaid02@gmail.com>
2025-03-26 22:30:54 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDVgX5siwAvifImpq4_u7T2-s3Wv0oso3g",
-  authDomain: "veronnovel-fd5c8.firebaseapp.com",
-  projectId: "veronnovel-fd5c8",
-  storageBucket: "veronnovel-fd5c8.firebasestorage.app",
-  messagingSenderId: "581899832569",
-  appId: "1:581899832569:web:423e84bb20e1aa54856628",
-  measurementId: "G-PK4WPW2LVB"
+  authDomain: "veronnovel-fd5b3.firebaseapp.com",
+  projectId: "veronnovel-fd5b3",
+  storageBucket: "veronnovel-fd5b3.appspot.com",
+  messagingSenderId: "436700764167",
+  appId: "1:436700764167:web:3dd9a3ae7b1bdfb32cb49b",
+  measurementId: "G-MGL7BXQSHZ"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
